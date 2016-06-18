7ef20282960ed8b0a818c54ed16538750ff613cf
duneding <maj.dagostino@gmail.com>
2016-06-18 16:09:48 -0300

    refactor test

--- test/scraper_test.exs
+++ test/scraper_test.exs
@@ -12,10 +12,6 @@ defmodule ScraperTest do
   end
 
   test "start supervised worker and get error" do
-    titles = [ "Lo más importante del día",
-      "Una nena ganó un concurso de princesas vestida de pancho",
-      "¿Es peligroso comer huevos todos los días?",
-      "¿Cómo maquillarte para una cita?"]
     {:ok, sup_pid} = Scraper.Supervisor.start_link
     {:ok, worker_pid} = Supervisor.start_child(sup_pid, [:server])
     assert Scraper.titles(:server, "http://localhost:3000/bar") == {:error, "Internal Server Error"}
